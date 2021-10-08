f79412524ae550d3c33031553ee6e9fd3e6c18c2
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2021-10-09 05:07:25 +0900

    Update Weighted-Uniform-Strings.cpp

--- Weighted-Uniform-Strings.cpp
+++ Weighted-Uniform-Strings.cpp
@@ -1,1 +1,34 @@
-
+vector<string> weightedUniformStrings(string s, vector<int> queries)
+{
+    vector<string> result;
+    std::map<int, bool> mp;
+    int check = 1;
+    
+    for (int i = 0; i < s.size(); ++i)
+    {
+        if (i+1 != s.size() && s[i+1] == s[i])
+        {
+            check++;
+        }
+        else
+        {
+            check = 1;
+        }
+            
+        mp[(s[i]-96) * check] = true;
+    }
+    
+    for (int i = 0; i < queries.size(); ++i)
+    {
+        if (mp[queries[i]] == true)
+        {
+            result.emplace_back("Yes");
+        }
+        else
+        {
+            result.emplace_back("No");
+        }
+    }
+    
+    return result;
+}
