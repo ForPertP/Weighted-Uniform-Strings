16ea86ecd6426425c312ca85c8820309f2fd19c7
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2025-03-31 03:42:52 +0900

    Update Weighted-Uniform-Strings.cpp

--- Weighted-Uniform-Strings.cpp
+++ Weighted-Uniform-Strings.cpp
@@ -91,24 +91,41 @@ int main()
     return 0;
 }
 
-string ltrim(const string &str) {
+string ltrim(const string &str)
+{
     string s(str);
-
     s.erase(
-        s.begin(),
-        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+        s.begin(), find_if(s.begin(), s.end(), [](unsigned char ch) { return !isspace(ch); })
     );
-
     return s;
 }
 
-string rtrim(const string &str) {
+string rtrim(const string &str)
+{
     string s(str);
-
     s.erase(
-        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
-        s.end()
+        find_if(s.rbegin(), s.rend(), [](unsigned char ch) { return !isspace(ch); }).base(), s.end()
     );
-
     return s;
+}
+
+std::vector<string> split(const string &str) {
+    vector<string> tokens;
+    string_view str_view(str);
+
+    size_t start = 0;
+    size_t end = 0;
+
+    while ((end = str_view.find(' ', start)) != string_view::npos) {
+        if (end > start) {
+            tokens.emplace_back(str_view.substr(start, end - start));
+        }
+        start = end + 1;
+    }
+
+    if (start < str_view.size()) {
+        tokens.emplace_back(str_view.substr(start));
+    }
+
+    return tokens;
 }
