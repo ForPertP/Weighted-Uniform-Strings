284d1110520838f0e4d96c10ef343a540be5195b
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2025-04-05 03:56:24 +0900

    Update Weighted-Uniform-Strings.cs

--- Weighted-Uniform-Strings.cs
+++ Weighted-Uniform-Strings.cs
@@ -58,37 +58,6 @@ class Result
     public static List<string> weightedUniformStrings2(string s, List<int> queries)
     {
         List<string> result = new List<string>();
-        Dictionary<int, bool> mp = new();
-        
-        int check = 1;
-        
-        for (int i = 0; i < s.size(); ++i)
-        {
-            check = 1;
-            
-            while (i + 1 < s.Length && s[i + 1] == s[i])
-            {
-                check++;
-                i++;
-            }
-            
-            for (int j = 1; j <= check; ++j)
-            {
-                mp[(s[i] - 'a' + 1) * j] = (true);
-            }
-        }
-        
-        foreach (int query in queries)
-        {
-            result.Add(mp.ContainsKey(query) ? "Yes" : "No");
-        }
-        
-        return result;
-    }    
-
-   public static List<string> weightedUniformStrings2(string s, List<int> queries)
-    {
-        List<string> result = new List<string>();
         SortedDictionary<int, bool> mp = new SortedDictionary<int, bool>();
 
         int check = 1;
@@ -115,6 +84,32 @@ class Result
         }
 
         return result;
-    }    
-    
+    }
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string s = Console.ReadLine();
+
+        int queriesCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> queries = new List<int>();
+
+        for (int i = 0; i < queriesCount; i++)
+        {
+            int queriesItem = Convert.ToInt32(Console.ReadLine().Trim());
+            queries.Add(queriesItem);
+        }
+
+        List<string> result = Result.weightedUniformStrings(s, queries);
+
+        textWriter.WriteLine(String.Join("\n", result));
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
 }
