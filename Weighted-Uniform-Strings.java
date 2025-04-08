24eaa5b04b8ec0a7b09fcabb61163f5dc40ff0ec
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2025-04-09 02:59:54 +0900

    Update Weighted-Uniform-Strings.java

--- Weighted-Uniform-Strings.java
+++ Weighted-Uniform-Strings.java
@@ -11,41 +11,17 @@ import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toList;
 
 class Result {
-  public static List<String> weightedUniformStrings(String s, List<Integer> queries) {
-    
-  }
-d
-    public static List<String> weightedUniformStrings(String s, List<Integer> queries)
-    {
-        List<String> result = new ArrayList<>();
-        Set<Integer> weights = new HashSet<>();
 
-        int check = 1;
+    /*
+     * Complete the 'weightedUniformStrings' function below.
+     *
+     * The function is expected to return a STRING_ARRAY.
+     * The function accepts following parameters:
+     *  1. STRING s
+     *  2. INTEGER_ARRAY queries
+     */
 
-        for (int i = 0; i < s.length(); ++i)
-        {
-            check = 1;
-            while (i + 1 < s.length() && s.charAt(i + 1) == s.cahrAt(i))
-            {
-                check++;
-                i++;
-            }
-
-            for (int j = 1; j <= check; ++j)
-            {
-                weights.add((s.charAt(i) - 'a' + 1) * j);
-            }
-        }
-
-        foreach (int query : queries)
-        {
-            result.add(weights.contains(query) ? "Yes" : "No");
-        }
-
-        return result;
-    }      
-
-public static List<String> weightedUniformStrings(String s, List<Integer> queries) {
+    public static List<String> weightedUniformStrings(String s, List<Integer> queries) {
         List<String> result = new ArrayList<>();
         Set<Integer> weights = new HashSet<>();
 
@@ -65,45 +41,13 @@ public static List<String> weightedUniformStrings(String s, List<Integer> querie
 
         for (int query : queries) {
             result.add(weights.contains(query) ? "Yes" : "No");
-        }
-
-        return result;
-    }  
-
-
-    public static List<String> weightedUniformStrings2(Sstring s, List<Integer> queries)
-    {
-        List<String> result = new ArrayList<>();
-        TreeMap<Integer, Boolean> mp = new TreeMap<>();
-
-        int check = 1;
-
-        for (int i = 0; i < s.length(); ++i)
-        {
-            check = 1;
-
-            while (i + 1 < s.length() && s.charAt( i + 1) == s.charAt(i))
-            {
-                check++;
-                i++;
-            }
-
-            for (int j = 1; j <= check; ++j)
-            {
-                mp.put((s.charAt(i) - 'a' + 1) * j, true);
-            }
-        }
-
-        foreach (int query : queries)
-        {
-            result.add(mp.containsKey(query) ? "Yes" : "No");
         }
 
         return result;
     }
-
-
-  public static List<String> weightedUniformStrings2(String s, List<Integer> queries) {
+    
+    
+    public static List<String> weightedUniformStrings2(String s, List<Integer> queries) {
         List<String> result = new ArrayList<>();
         TreeMap<Integer, Boolean> mp = new TreeMap<>();
 
@@ -127,5 +71,38 @@ public static List<String> weightedUniformStrings(String s, List<Integer> querie
         }
 
         return result;
-    }      
+    }    
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s = bufferedReader.readLine();
+
+        int queriesCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> queries = IntStream.range(0, queriesCount).mapToObj(i -> {
+            try {
+                return bufferedReader.readLine().replaceAll("\\s+$", "");
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        })
+            .map(String::trim)
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        List<String> result = Result.weightedUniformStrings(s, queries);
+
+        bufferedWriter.write(
+            result.stream()
+                .collect(joining("\n"))
+            + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
 }
