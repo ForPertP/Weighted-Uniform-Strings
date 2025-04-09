376e27c676a14ca2b818831c0128aff473f4ef7c
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2025-04-10 03:03:59 +0900

    Update Weighted-Uniform-Strings.java

--- Weighted-Uniform-Strings.java
+++ Weighted-Uniform-Strings.java
@@ -74,6 +74,7 @@ class Result {
     }    
 }
 
+
 public class Solution {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
